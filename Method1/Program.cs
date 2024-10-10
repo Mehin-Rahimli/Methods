3200fa874a1243ab50ed7bee8ac2fb06c6752903
Mehin-Rahimli <mehincr-bp217@code.edu.az>
2024-10-10 01:53:51 -0700

    Initial commit

---
+++ Method1/Program.cs
@@ -0,0 +1,64 @@
+﻿
+#region Task1
+//Verilmis arrayin en kicik elementini tapin
+//namespace Method1
+//{
+//    internal class program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[] array = { 0, 1, 2, 3, 4, 5, 6, 7 };
+//            Console.WriteLine(GetSmallest(array));
+//        }
+//        public static int GetSmallest(int[] arr)
+//        {
+//            int min = arr[0];
+//            for (int i = 0; i < arr.Length; i++)
+//            {
+//                if (arr[i] < min)
+//                {
+//                    min = arr[i];
+//                }
+//            }
+//            return min;
+//        }
+//    }
+//}
+#endregion
+
+
+
+#region Task2
+//overload methodlar ile fiqurlarin sahesini tapin.
+//namespace Method1
+//{
+//    internal class program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Area(2);
+//            Area(1.5, 2);
+//            Area(2, 2, 1.5);
+//            Area(1, 1, 2,1);
+//        }
+//        public static void Area(double radius)
+//        {
+//            Console.WriteLine("S=pi*r**2=" + (3 * radius * radius));
+//        }
+//        public static void Area(double a, double b)
+//        {
+//            Console.WriteLine("S=a*b=" + (a * b));
+//        }
+//        public static void Area(double a, double b, double c)
+//        {
+//            Console.WriteLine("S=2(a*b+a*c+b*c)=" + (2 * (a * b + a * c + b * c)));
+//        }
+//        public static void Area(double a, double b, double c,double r)
+//        {
+
+//            Console.WriteLine("S=p*r,p=(a+b+c)/2,S=r*p=" + r*((a+b+c)/2));
+//        }
+//    }
+//}
+
+#endregion
